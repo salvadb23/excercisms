2847ad564cb414209edc1321d4f167c88d1274ff
salvador becerra <salbecerra1023@gmail.com>
2019-07-06 13:07:03 -0700

    breakout

---
+++ breakout.js
@@ -0,0 +1,13 @@
+function find_middle(ll){
+    middle = ll.count // 2
+    count = 0
+    current_node = ll.head
+    while (current_node != Null){
+        if (counter= mid_num){
+            break}
+        current_node = current_node.next
+        counter += 1
+    }
+
+    return (current_node.data, current_node.next.data)
+}
